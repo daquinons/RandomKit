3dabe2e7ba5e58accfc7f2e1047da640ab9dd75a
David Lechón Quiñones <dalequi@gmail.com>
2015-10-08 14:25:09 -0400

    Creating Xcode project

---
+++ RandomKit/RandomKit.h
@@ -0,0 +1,19 @@
+//
+//  RandomKit.h
+//  RandomKit
+//
+//  Created by David Lechón Quiñones on 08/10/15.
+//  Copyright © 2015 dlqapps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RandomKit.
+FOUNDATION_EXPORT double RandomKitVersionNumber;
+
+//! Project version string for RandomKit.
+FOUNDATION_EXPORT const unsigned char RandomKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RandomKit/PublicHeader.h>
+
+
